cfd3d56d340112bb7680820a3417d6e1ae31df3c
viezer-robert-30125 <79532810+viezer-robert-30125@users.noreply.github.com>
2021-05-18 19:22:36 +0300

    Add files via upload

---
+++ AirplaneTicket.java
@@ -0,0 +1,28 @@
+package aut.utcluj.isp.ex3;
+
+/**
+ * @author stefan
+ */
+public class AirplaneTicket {
+    private String id;
+    private Double price;
+    private String destination;
+
+    public AirplaneTicket(String id, Double price, String destination) {
+        this.id = id;
+        this.price = price;
+        this.destination = destination;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+}
